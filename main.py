e0b04b0926570aedcfea3f4a736301fab10679c8
yonzro1610 <yonzro1610@proton.me>
2024-05-25 18:23:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import requests
+import json
+
 UID = input("Enter a user ID > ")
+UID2 = input("A second > ")
+
+payload = {
+    "userIds": [UID, UID2]
+}
 
 URLS = {
     "checkPresence": "https://presence.roblox.com/v1/presence/users",
     "checkFriendsPresence": f"https://friends.roblox.com/v1/users/{UID}/friends?userSort=0"
 }
 
+response = requests.post(URLS["checkPresence"], json=payload)
+
+print(response.json())
